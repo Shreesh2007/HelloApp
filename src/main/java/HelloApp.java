265db8d0a595cfb9ab146b6e7c41cbf78954fa5f
Shreesh2007 <sp0888@srmist.edu.in>
2026-04-01 20:02:27 +0530

    UC4: Display "Hello" with Multiple CLA or Default Message

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,3 +1,14 @@
-public class HelloApp {
-   
+public class HelloApp{
+public static void main(String[] args){
+String name="World";
+if(args.length>0){
+StringBuilder NameBuilder=new StringBuilder();
+for(int i=0;i<args.length;i++){
+NameBuilder.append(args[i]);
+if(i<args.length-1) NameBuilder.append(", ");
+}
+name=NameBuilder.toString();
+}
+System.out.println("Hello, "+name+"!");
+}
 }
